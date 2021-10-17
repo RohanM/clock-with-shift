08cad453a6a414f1e9096f63f044b7c23d8c60be
Rohan Mitchell <rohan@rohanmitchell.com>
2021-10-17 17:43:26 +1100

    Add test to run loop

--- test.cpp
+++ test.cpp
@@ -22,8 +22,35 @@ void test_setup_initial_values() {
   assert(time_between_outs == 0);
 }
 
+void run_loop(int gate, long duration) {
+    setDigitalValue(CLOCK_IN, gate);
+    loop();
+    advanceTime(duration);
+}
+
+void test_loop_running() {
+  setAnalogValue(UPPER_POT, 499);
+  setAnalogValue(MIDDLE_POT, 250);
+  setAnalogValue(LOWER_POT, 0);
+
+  setup();
+
+  for(int i=0; i<10; i++) {
+    std::cout << "\n\n";
+    run_loop(1, 100);
+    std::cout << "\n\n";
+    run_loop(0, 100);
+    std::cout << "\n\n";
+    run_loop(1, 100);
+  }
+}
+
 int main() {
+  std::cout << "test_setup_initial_values()\n";
   test_setup_initial_values();
+
+  std::cout << "\n\ntest_loop_running()\n";
+  test_loop_running();
 
   return 0;
 }
