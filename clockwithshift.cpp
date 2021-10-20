c36cac37576d320c16db2b753885888a4d0b6ec3
Rohan Mitchell <rohan@rohanmitchell.com>
2021-10-20 15:43:34 +1100

    Remove old loop() logic

--- clockwithshift.cpp
+++ clockwithshift.cpp
@@ -32,24 +32,13 @@ Code by a773 (atte.dk) and released under the GPL licence
 #define UNSHIFTED_OUT     10
 
 long now = 0;
-long time_between_outs = 0;
-long time_between_unshifted_trigger_out = 0;
-int  nb_triggs = 0;
-int  nb_unshifted_triggs = 0;
 
-//offbeat shift stuff:
-int offbeat_input = 5; //analog input 5
-unsigned long scheduledshiftbegin = 0;
-int delaystart = 0;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //variables for reading the input knobs faster than at each beat
-//last values we read from them:
-int lastupper = 0;
 //last time we read the knobs: (we need to only read them so often or the loop will be too slow)
 unsigned long lastknobread = 0;
 unsigned long knobreadinginterval = 500;
-//wow ok this is getting messy, but this is adapted code from someone else, we'll do a clean at some point
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -57,9 +46,11 @@ unsigned long knobreadinginterval = 500;
  * Return the duration for an output trigger to remain high.
  * This defaults to TRIGGER_LENGTH, but will reduce if our frequency is sufficiently high.
  */
+/*
 int trigger_length() {
   return min(TRIGGER_LENGTH, time_between_outs / 2);
 }
+*/
 
 
 /**
@@ -188,7 +179,7 @@ public:
     mult_reading = analogRead(UPPER_POT);
     div_reading = analogRead(MIDDLE_POT);
     mode_reading = analogRead(LOWER_POT);
-    beatshift = analogRead(offbeat_input);
+    beatshift = analogRead(OFFBEAT_IN);
   }
 
   void updateSettings(bool edge) {
@@ -268,176 +259,18 @@ void loop()
 
   controls.updateSettings(gateReader.edge);
 
-  // setup mult and div
-  if(gateReader.edge && gateReader.getting_triggers){
-    // only update if triggers faster than 2.5 seconds
-    time_between_outs = gateReader.time_between_ins / controls.get_mult();
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  // We'll only read our analog input knobs every so often so we don't slow down the timing loop:
-  // Let's call this area the brain = burned toast zone
-  // We use this to also detect if knobs have been changed and recalculate beat timing without losing
-  // rhythm (hopefully)
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-  if ( (millis() - lastknobread) >= knobreadinginterval){
-
+  if (now - lastknobread >= knobreadinginterval) {
     controls.update();
 
-    //if the upper knob has changed much:
-    if ( (controls.mult_reading - lastupper) > 20 || (controls.mult_reading - lastupper < -20) ){
-      lastupper = controls.mult_reading;
-
-      //only do this recalc if we're not currently working on an incoming beat:
-      if (!gateReader.edge){
-
-      //recalculate the time between outs cut into pieces by the multiplier
-      time_between_outs = gateReader.time_between_ins / controls.get_mult();
-      //now we need to recalculate our triggers to keep synced with tempo:
-
-
-      //we'll divide the time since the last beat input by the time between outs,
-      //and round down to the nearest number to say what number trigger we should have recently passed
-      nb_unshifted_triggs = 1 + floor( (millis() - gateReader.last_trigger_in) / ( (time_between_outs) * controls.get_div()) );
-
-      //now we'll fake having triggered the last time so we keep in tempo:
-      // TODO: This is terrible reaching into Trigger like this. Refactor.
-      unshiftedTrigger.last_trigger_out = gateReader.last_trigger_in + ((nb_unshifted_triggs-1) * (time_between_outs * controls.get_div()));
-
-      Serial::print("unshifted trigs: ");
-      Serial::println(nb_unshifted_triggs);
-
-      Serial::print("last unshifted trigger out: ");
-      Serial::print(unshiftedTrigger.last_trigger_out);
-      Serial::print("             Current time: ");
-      Serial::println(millis());
-      Serial::print("Calculated gap: ");
-      Serial::println((time_between_outs * controls.get_div()));
-
-
-      if (controls.beatshift < 30){  //if we're not shifting:
-        //make the beatshift output match the regular unshifted output:
-        nb_triggs = nb_unshifted_triggs;
-        // TODO: This is terrible reaching into Trigger like this. Refactor.
-        shiftedTrigger.last_trigger_out = unshiftedTrigger.last_trigger_out;
-      }
-
-      //if we are shifting:
-      else if (controls.beatshift >= 30 && delaystart == 0){ //if we're shifting the beat and we're not currently delaying the first output:
-        //we want to:
-        // number of triggers = round down to nearest whole number: [ current time - (last trigger in + shift amount) ] / (time_between_outs * controls.get_div()
-        nb_triggs = floor( (millis()- (gateReader.last_trigger_in + ((time_between_outs * controls.get_div()) * (float(map(controls.beatshift, 30, 1023, 0, 99)) / 100)) )) / time_between_outs);
-        //now we'll fake having triggered the last time so we keep in tempo:
-        // last trigger out = number of triggers completed x time between outs (adjusted by divider knob) + shifting offset
-        shiftedTrigger.last_trigger_out = gateReader.last_trigger_in + (nb_triggs * (time_between_outs * controls.get_div())) + ((time_between_outs * controls.get_div()) * (float(map(controls.beatshift, 30, 1023, 0, 99)) / 100));
-      }
-
-      else if (controls.beatshift >= 30 && delaystart == 1){ //if we're shifting the beat, but we also haven't yet hit the first scheduled delay beat:
-        //we want to:
-        //
-        scheduledshiftbegin = gateReader.last_trigger_in + ((time_between_outs * controls.get_div()) * (float(map(controls.beatshift, 30, 1023, 0, 99)) / 100) );
-      }
-
-      //nb_unshifted_triggs++;
-      //nb_triggs++;
-      }
-
-    }
-
-
-
-  }
-  ///////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-  ////////////////////////////////////////////////////////////////////////////////
-  // if we're getting sensible time (above zero) and we're not stopped,
-  // then it's time to process triggers:
-  ///////////////////////////////////////////////////////////////////////////////
-  if(gateReader.time_between_ins > 0 && !controls.stopped){
-  /*  if(nb_unshifted_triggs < 1){
-      Serial::print("                                     FIRE! ");
-     Serial::println(nb_unshifted_triggs);
-    }
-  */
-    /*if( (last_unshifted_trigger_out - millis()) > time_between_outs){
-      //Serial::println("                            WHY ARE WE WAITING!?");
-      Serial::print("time between outs: ");
-      Serial::println(time_between_outs);
-      Serial::print("last trigger out: ");
-      Serial::println(last_unshifted_trigger_out);
-      Serial::print("       triggs: ");
-      Serial::println(nb_unshifted_triggs);
-    }*/
-
-
-    ////////////////////////////////////
-    //let's do the unshifted elements:
-    ////////////////////////////////////
-    if(nb_unshifted_triggs <= 1 && gateReader.edge){
-      unshiftedTrigger.trigger(trigger_length());
-      nb_unshifted_triggs = controls.get_mult();
-    }
-    else if( (millis() - unshiftedTrigger.last_trigger_out) >= time_between_outs ){
-       if(nb_unshifted_triggs >= 1){
-         unshiftedTrigger.trigger(trigger_length());
-         nb_unshifted_triggs--;
-         //last_unshifted_trigger_out = millis();
-         Serial::print("       triggs: ");
-         Serial::println(nb_unshifted_triggs);
-         Serial::print("last trigger out: ");
-         Serial::println(unshiftedTrigger.last_trigger_out);
-         Serial::print("current time: ");
-         Serial::println(millis());
-       }
-     }
-
-
-    /////////////////////////////////
-    //let's do the shifted elements:
-    /////////////////////////////////
-    if(nb_triggs <= 1 && gateReader.edge){
-      //let's trigger on the beat if we've set the offbeat knob to zero
-      //(with a tiny bit of slack at the bottom in case the potentiometer isn't perfect):
-      if(controls.beatshift < 30){
-        delaystart = 0;
-        shiftedTrigger.trigger(trigger_length());
-        nb_triggs = controls.get_mult();
-      }
-      //however, if the shift knob is turned up, let's schedule the first trigger according to shift time
-      else if (delaystart == 0 && controls.beatshift >= 30){
-        //set this variable so we can trigger the delay start
-        delaystart = 1;
-        //time to start       current time + [ ( time between outs x division knob ) x fraction of 1 output beat (shift between 0 and 1)
-        scheduledshiftbegin = millis() + ((time_between_outs * controls.get_div()) * (float(map(controls.beatshift, 30, 1023, 0, 99)) / 100) );
-      }
-    }
-
-    //we want to only trigger when it's been the trigger time, multiplied by a
-    //mapping of the input from 30-1023, to 1-2 so we should get a multiplication between 1 and 2
-    //which in effect turns our input potentiometer into a way to phase shift between being on beat, to being a beat behind
-    else if ( (now - shiftedTrigger.last_trigger_out) >=  (time_between_outs * controls.get_div()) ){
-      if(nb_triggs > 1){
-        shiftedTrigger.trigger(trigger_length());
-        nb_triggs--;
-      }
-    }
-  ////////////////////////////////////////////
-  //some logic to handle delayed first beat:
-  ////////////////////////////////////////////
-  if (delaystart == 1){
-    if (millis() >= scheduledshiftbegin){
-      shiftedTrigger.trigger(trigger_length());
-      nb_triggs = controls.get_mult();
-      delaystart = 0;
-    }
-  }
+    // ...
   }
 
 
-  ///////////////////////////////////////////////////////////////////////////////
-  //Trigger handling
-  /////////////////////////////
+  // Fire triggers
+  // if(gateReader.time_between_ins > 0 && !controls.stopped){
+  // }
+
+  // Trigger update
   unshiftedTrigger.update(now);
   shiftedTrigger.update(now);
 }
